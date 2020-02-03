c60a1ab8def4ae25cc8e2da3940aabc0dc6ed185
pfernandes <phelipe.fern@gmail.com>
2020-02-03 15:03:03 -0500

    fix startup bugs

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   context: __dirname,
-  entry: "./frontend/splitly.jsx",
+  entry: "./app/frontend/splitly.jsx",
   output: {
     path: path.join(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js"
