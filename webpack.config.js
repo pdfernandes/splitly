4f5db67fa8955482a697a218ec6040a5dd8792d6
pfernandes <phelipe.fern@gmail.com>
2020-02-03 14:36:30 -0500

    frontend and webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/splitly.jsx",
+  output: {
+    path: path.join(__dirname, "app", "assets", "javascripts"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  },
+  module: {
+    rules: [
+      {
+        parser: {
+          amd: false
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          query: {
+            presets: ["@babel/env", "@babel/react"]
+          }
+        }
+      }
+    ]
+  },
+  devtool: "eval-source-map"
+};
