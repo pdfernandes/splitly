d5e5a0656015776c09a667eafe1fb4e8b5ba57e7
pfernandes <phelipe.fern@gmail.com>
2020-02-03 20:27:52 -0500

    begin hooks refactor

--- frontend/splitly.jsx
+++ frontend/splitly.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+import Root from "./components/root";
 
 import { login, signup, logout } from '../frontend/util/session_api_utils'
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to Splitly</h1>, root);
-  window.login = login;
-  window.logout = logout;
-  window.signup = signup
+  // window.login = login;
+  // window.logout = logout;
+  // window.signup = signup;
+  const store = configureStore()
+  ReactDOM.render(<Root store={store} />, root);
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
 });
